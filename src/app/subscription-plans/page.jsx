e4ec037ed03ba32683c37a6ec6a361ce81084c6e
immadsid <immadsiddiqui86@gmail.com>
2024-03-12 02:08:55 +0500

    Complete home page and subscription page

--- src/app/subscription-plans/page.jsx
+++ src/app/subscription-plans/page.jsx
@@ -1,7 +1,199 @@
 import React from "react";
+import Image from "next/image";
+import Button from "@/components/ui/Button";
 
 const SubscriptionPlan = () => {
-  return <div className="h-screen">Subscription Plan Page</div>;
+  return (
+    <div className="h-full w-full">
+      <div className="mx-4 my-12 grid grid-cols-1 gap-8 md:mx-20 md:grid-cols-3">
+        <div className="p-8">
+          <div className="border-b border-gray-200 pb-8">
+            <h className="text-4xl font-semibold">Basic</h>
+            <p className="pt-3 font-semibold text-gray-700">3 Cases</p>
+          </div>
+          <div className="border-b border-gray-200 py-6">
+            <p className="text-sm font-semibold">Payment Options</p>
+            <ul className="ml-4 list-disc text-sm font-semibold tracking-wider text-gray-700">
+              <li className="pt-3">Annual — $18,000/year </li>
+            </ul>
+          </div>
+          <div className="my-3 border-b border-gray-200 pb-8">
+            <Image
+              src="/images/Pulmonary-Hypertrophy-image.webp"
+              alt="Pulmonary-Hypertrophy"
+              width={500}
+              height={500}
+              objectFit="contain"
+            />
+          </div>
+          <div className="border-b border-gray-200 pb-8 pt-2">
+            <p className="font-semibold tracking-wide">
+              Covers 3 Complete Cases From Beginning To End. 3 Trial Or
+              Mediation Presentations, Which Can Include:
+            </p>
+          </div>
+          <div className="py-6">
+            <ul className="ml-4 list-disc tracking-wider text-gray-700">
+              <li className="pt-3 text-sm font-semibold">
+                Two 3D animations per case{" "}
+              </li>
+              <li className="pt-3 text-sm font-semibold">
+                Medical Illustrations
+              </li>
+              <li className="pt-3 text-sm font-semibold">
+                Medical Illustrations{" "}
+              </li>
+              <li className="pt-3 text-sm font-semibold">Custom Graphics </li>
+              <li className="pt-3 text-sm font-semibold">Animated Graphics </li>
+              <li className="pt-3 text-sm font-semibold">
+                Interactive Timelines{" "}
+              </li>
+              <li className="pt-3 text-sm font-semibold">
+                Interactive Linking Presentations{" "}
+              </li>
+              <li className="pt-3 text-sm font-semibold">Custom Video Work </li>
+              <li className="pt-3 text-sm font-semibold">Custom Audio Work </li>
+              <li className="pt-3 text-sm font-semibold">
+                3D Radiological Imagery{" "}
+              </li>
+            </ul>
+          </div>
+          <div>
+            <Button
+              name={"Continue"}
+              type={"button"}
+              className={
+                "min-w-full bg-gradient-to-br from-red-900 to-zinc-900 text-white"
+              }
+            />
+          </div>
+        </div>
+
+        <div className="rounded-3xl border border-gray-200 p-8">
+          <div className="border-b border-gray-200 pb-8">
+            <h className="text-4xl font-semibold">Pro</h>
+            <p className="pt-3 font-semibold text-gray-700">5 Cases</p>
+          </div>
+          <div className="border-b border-gray-200 py-6">
+            <p className="text-sm font-semibold">Payment Options</p>
+            <ul className="ml-4 list-disc text-sm font-semibold tracking-wider text-gray-700">
+              <li className="pt-3">Annual — $25,000/year</li>
+            </ul>
+          </div>
+          <div className="my-3 border-b border-gray-200 pb-8">
+            <video autoPlay loop muted id="video" className="object-contain">
+              <source src="/videos/video-1.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+          <div className="border-b border-gray-200 pb-8 pt-2">
+            <p className="font-semibold tracking-wide">
+              Covers 5 Complete Cases From Beginning To End. 5 Trial Or
+              Mediation Presentations, Which Can Include:
+            </p>
+          </div>
+          <div className="py-6">
+            <ul className="ml-4 list-disc tracking-wider text-gray-700">
+              <li className="pt-3 text-sm font-semibold">
+                Two 3D animations per case{" "}
+              </li>
+              <li className="pt-3 text-sm font-semibold">
+                Medical Illustrations
+              </li>
+              <li className="pt-3 text-sm font-semibold">
+                Medical Illustrations{" "}
+              </li>
+              <li className="pt-3 text-sm font-semibold">Custom Graphics </li>
+              <li className="pt-3 text-sm font-semibold">Animated Graphics </li>
+              <li className="pt-3 text-sm font-semibold">
+                Interactive Timelines{" "}
+              </li>
+              <li className="pt-3 text-sm font-semibold">
+                Interactive Linking Presentations{" "}
+              </li>
+              <li className="pt-3 text-sm font-semibold">Custom Video Work </li>
+              <li className="pt-3 text-sm font-semibold">Custom Audio Work </li>
+              <li className="pt-3 text-sm font-semibold">
+                3D Radiological Imagery{" "}
+              </li>
+            </ul>
+          </div>
+          <div>
+            <Button
+              name={"Continue"}
+              type={"button"}
+              className={
+                "min-w-full bg-gradient-to-br from-red-900 to-zinc-900 text-white"
+              }
+            />
+          </div>
+        </div>
+
+        <div className="p-8">
+          <div className="border-b border-gray-200 pb-8">
+            <h className="text-4xl font-semibold">Platinum</h>
+            <p className="pt-3 font-semibold text-gray-700">8 Cases</p>
+          </div>
+          <div className="border-b border-gray-200 py-6">
+            <p className="text-sm font-semibold">Payment Options</p>
+            <ul className="ml-4 list-disc text-sm font-semibold tracking-wider text-gray-700">
+              <li className="pt-3">Annual — $32,000/year </li>
+            </ul>
+          </div>
+          <div className="my-3 border-b border-gray-200 pb-8">
+            <Image
+              src="/images/spine-illustration-image.webp"
+              alt="spine-illustration"
+              width={500}
+              height={500}
+              objectFit="contain"
+            />
+          </div>
+          <div className="border-b border-gray-200 pb-8 pt-2">
+            <p className="font-semibold tracking-wide">
+              Covers 8 Complete Cases From Beginning To End. 8 Trial Or
+              Mediation Presentations, Which Can Include:
+            </p>
+          </div>
+          <div className="py-6">
+            <ul className="ml-4 list-disc tracking-wider text-gray-700">
+              <li className="pt-3 text-sm font-semibold">
+                Two 3D animations per case{" "}
+              </li>
+              <li className="pt-3 text-sm font-semibold">
+                Medical Illustrations
+              </li>
+              <li className="pt-3 text-sm font-semibold">
+                Medical Illustrations{" "}
+              </li>
+              <li className="pt-3 text-sm font-semibold">Custom Graphics </li>
+              <li className="pt-3 text-sm font-semibold">Animated Graphics </li>
+              <li className="pt-3 text-sm font-semibold">
+                Interactive Timelines{" "}
+              </li>
+              <li className="pt-3 text-sm font-semibold">
+                Interactive Linking Presentations{" "}
+              </li>
+              <li className="pt-3 text-sm font-semibold">Custom Video Work </li>
+              <li className="pt-3 text-sm font-semibold">Custom Audio Work </li>
+              <li className="pt-3 text-sm font-semibold">
+                3D Radiological Imagery{" "}
+              </li>
+            </ul>
+          </div>
+          <div>
+            <Button
+              name={"Continue"}
+              type={"button"}
+              className={
+                "min-w-full bg-gradient-to-br from-red-900 to-zinc-900 text-white"
+              }
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SubscriptionPlan;
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -135,43 +135,334 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="relative flex h-screen flex-col justify-center">
-        <video autoPlay loop muted id="video" className=" z-[-1] object-cover">
+      <div className=" flex h-screen w-full flex-col justify-center">
+        <video autoPlay loop muted id="video" className="m-0 object-cover p-0">
           <source src="/videos/overview-of-injuries.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-        <div className="absolute inset-0 top-[200px] flex flex-col items-center justify-end">
-          <div className="flex items-center justify-center gap-6">
-            <Link href="#">
-              <Button
-                name={"View Presentation"}
-                type={"button"}
-                className={"border border-white bg-black text-white"}
-              />
-            </Link>
-            <p className="text-white">
-              You may have een a Powerpoint before. However you havn’t seen
-              anything like the ones that we design. The jury should be able to
-              understand the evidence and the legal arguments without getting
-              bogged down in technical details. Second, the presentations should
-              be persuasive. The lawyer should use clear and powerful language
-              to convince the jury that the plaintiff is entitled to
-              compensation. Third, the presentation should be visually appealing
-              and hold the attention of the jury.
-            </p>
+        <div className="flex w-full flex-col items-end justify-center bg-black py-12 pb-48">
+          <div className="w-full max-w-6xl px-12 lg:px-12">
+            <div className="flex flex-col items-center justify-between gap-6 lg:flex-row">
+              <Link href="#">
+                <Button
+                  name={"View Presentation"}
+                  type={"button"}
+                  className={"border border-white bg-black text-white"}
+                />
+              </Link>
+              <p className="text-sm text-white lg:w-2/3 lg:text-base">
+                You may have been a Powerpoint before. However you haven’t seen
+                anything like the ones that we design. The jury should be able
+                to understand the evidence and the legal arguments without
+                getting bogged down in technical details. Second, the
+                presentations should be persuasive. The lawyer should use clear
+                and powerful language to convince the jury that the plaintiff is
+                entitled to compensation. Third, the presentation should be
+                visually appealing and hold the attention of the jury.
+              </p>
+            </div>
           </div>
         </div>
       </div>
-      <div className="grid grid-cols-3 bg-white">
-        <div className="col-span-1">Left Section</div>
-        <div className="col-span-2">
+
+      <div className="mt-0 grid grid-cols-1 bg-white lg:grid-cols-3 xl:mt-20">
+        <div className="col-span-1 lg:col-span-1">
+          <div className="m-10 ">
+            <div className="border-b border-gray-200 pb-6">
+              <p className="font-semibold uppercase tracking-widest text-red-950">
+                Trial Template
+              </p>
+              <h1 className="py-3 text-4xl font-semibold tracking-wide">
+                We Prepare You From The Begining{" "}
+              </h1>
+            </div>
+            <div className="border-b border-gray-200  py-6">
+              <h1 className="py-3 text-2xl font-semibold tracking-wide">
+                Demand
+              </h1>
+              <p className="text-sm font-semibold text-gray-600">
+                We Create Graphics For Your Demand letters that will help to
+                explain complex concepts and increase clarity.{" "}
+              </p>
+            </div>
+            <div className="border-b border-gray-200  py-6">
+              <h1 className="py-3 text-2xl font-semibold tracking-wide">
+                Depos
+              </h1>
+              <p className="text-sm font-semibold text-gray-600">
+                Medical illustrations, animations and graphics for your expert
+                depos that will come in as important exhibits
+              </p>
+            </div>
+            <div className="border-b border-gray-200  py-6">
+              <h1 className="py-3 text-2xl font-semibold tracking-wide">
+                Mediation
+              </h1>
+              <p className="text-sm font-semibold text-gray-600">
+                Mediation is an important deadline and we create entire trial
+                presentations for mediation to increase your case value.
+              </p>
+            </div>
+            <div className="border-b border-gray-200  py-6">
+              <h1 className="py-3 text-2xl font-semibold tracking-wide">
+                Time
+              </h1>
+              <p className="text-sm font-semibold text-gray-600">
+                Since we do not bill hourly, we tend to accomplish our work
+                faster than you might have come to expect.
+              </p>
+            </div>
+            <div className="py-6">
+              <h1 className="py-3 text-2xl font-semibold tracking-wide">
+                Trial
+              </h1>
+              <p className="text-sm font-semibold text-gray-600">
+                Other than animations, we create interactive editable
+                presentations for openings, cross examinations and closing.
+              </p>
+            </div>
+            <div className="flex flex-col gap-3 py-6">
+              <Link href="#">
+                <Button
+                  name={"Explore"}
+                  type={"button"}
+                  className={"min-w-full border-2 border-black bg-white"}
+                />
+              </Link>
+              <Link href="#">
+                <Button
+                  name={"Schedule A Demo"}
+                  type={"button"}
+                  className={" min-w-full bg-gray-200"}
+                />
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1 lg:col-span-2">
           <Image
             src="/images/ayers-rd-animation.webp"
             alt="ayers-rd-animation"
-            width={1000}
-            height={1000}
-            className="object-cover"
+            width={500}
+            height={500}
+            className="h-full w-full object-cover"
           />
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2">
+        <div className="order-1 flex items-center justify-center">
+          <Image
+            src="/images/scaled.webp"
+            alt="skull"
+            width={500}
+            height={500}
+            className="h-full w-full object-cover"
+          />
+        </div>
+        <div className="order-2 flex flex-col justify-center bg-gradient-to-br from-red-950 to-zinc-900 px-16 py-16 text-white">
+          <h1 className="py-3 text-2xl font-semibold tracking-wide">
+            We&apos;re Creative With Medical Records
+          </h1>
+          <p className="text-sm font-semibold text-white">
+            CT scans are objective. They are created by machines and are not
+            subject to the interpretation of a medical illustrator. This can
+            make them more persuasive to juries, who are often skeptical of
+            expert testimony.
+          </p>
+        </div>
+
+        <div className="order-4 flex flex-col justify-center bg-gradient-to-br from-red-950 to-zinc-900 px-16 py-16 text-white sm:order-3">
+          <h1 className="py-3 text-2xl font-semibold tracking-wide">
+            From Flat Slices To An Animated 3D Model
+          </h1>
+          <p className="text-sm font-semibold text-white">
+            CT Scans are more dynamic. They can be rotated and viewed from
+            different angles. They can output to video and rotated around. This
+            allows the jury to see the injuries from different perspectives and
+            to better understand how they occurred.
+          </p>
+        </div>
+        <div className="order-3 flex items-center justify-center sm:order-4">
+          <Image
+            src="/images/leg-image.webp"
+            alt="skull"
+            width={500}
+            height={500}
+            className="h-full w-full object-cover"
+          />
+        </div>
+        <div className="order-5 flex h-full w-full items-center justify-center bg-black">
+          <video autoPlay loop muted id="video" className="object-cover">
+            <source src="/videos/face-to-ct-scan.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <div className="order-6 flex flex-col justify-center bg-gradient-to-br from-red-950 to-zinc-900 px-16 py-16 text-white">
+          <h1 className="py-3 text-2xl font-semibold tracking-wide">
+            Humanize The Injuries With A Face
+          </h1>
+          <p className="text-sm font-semibold text-white">
+            If the person being scanned is scared, in pain, unconscious or
+            crying, the CT Scan will pick up those important details which will
+            help others empathize with their pain rather than look at the
+            injuries as they would a skeleton in some biology class.
+          </p>
+        </div>
+      </div>
+
+      {/* <div>
+        <div>
+          <video
+            autoPlay
+            loop
+            muted
+            id="video"
+            className="z-[-1] w-full max-w-none"
+          >
+            <source src="/videos/car-crash-animation.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <div className="flex w-full flex-col items-end justify-center py-12 pb-48">
+          <div className="w-full max-w-6xl px-12 lg:px-12">
+            <div className="flex flex-col items-center justify-between gap-6 lg:flex-row">
+              <Link href="#">
+                <Button
+                  name={"View Animations"}
+                  type={"button"}
+                  className={"border border-white bg-black text-white"}
+                />
+              </Link>
+              <p className="text-sm text-black lg:w-2/3 lg:text-base">
+                You may have been a Powerpoint before. However you haven’t seen
+                anything like the ones that we design. The jury should be able
+                to understand the evidence and the legal arguments without
+                getting bogged down in technical details. Second, the
+                presentations should be persuasive. The lawyer should use clear
+                and powerful language to convince the jury that the plaintiff is
+                entitled to compensation. Third, the presentation should be
+                visually appealing and hold the attention of the jury.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div> */}
+
+      <div className="relative">
+        <div className="">
+          <video
+            autoPlay
+            loop
+            muted
+            id="video"
+            className="z-[-1] w-full max-w-none"
+          >
+            <source src="/videos/car-crash-animation.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <div className="absolute inset-0 flex items-end justify-center">
+          <div className="flex w-full flex-col items-end justify-center py-12">
+            <div className="w-full max-w-6xl px-12 lg:px-12">
+              <div className="flex flex-col items-center justify-between lg:flex-row">
+                <Link href="#">
+                  <Button
+                    name={"View Animations"}
+                    type={"button"}
+                    className={
+                      "border border-white bg-transparent/40 text-white"
+                    }
+                  />
+                </Link>
+                <p className="text-sm text-white lg:w-2/3 lg:text-base">
+                  3D Animations can be a powerful tool in personal injury cases.
+                  They can be used to recreate the accident scene, show the path
+                  of travel of the vehicles involved, and the injuries
+                  sustained. Animations can also be used to explain complex
+                  scientific or medical concepts. They are an important part of
+                  a successful case, and now at long last, they are affordable.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="relative h-screen bg-black">
+        <Image
+          src="/images/timelines-image.webp"
+          alt="Timelines"
+          layout="fill"
+          objectFit="contain"
+          className="z-0"
+        />
+
+        <div className="z-10 ml-20 mt-14 w-[450px]">
+          <h1 className="py-3 text-4xl font-semibold tracking-wide text-white">
+            Illustrations
+          </h1>
+        </div>
+        <div className="absolute bottom-0 right-0 z-10 mx-10 max-w-4xl pb-6">
+          <div className="w-full">
+            <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+              <Link href="#">
+                <Button
+                  name={"View Illustration"}
+                  type={"button"}
+                  className={"border border-white bg-black text-white"}
+                />
+              </Link>
+              <p className="text-sm text-white">
+                A timeline is a powerful tool that can be used to show the jury
+                the extent of a persons injuries and the amount of treatment
+                they have undergone. The timeline can show the dates of the
+                injuries, the types of treatment. This information can be very
+                helpful to understanding the severity of the injuries and the
+                impact that they have had on someones life. When these timelines
+                hyperlink to the actual treatment documents these timelines
+                become an essential part of trial.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="relative h-screen">
+        <Image
+          src="/images/Pulmonary-Hypertrophy-image.webp"
+          alt="Timelines"
+          layout="fill"
+          objectFit="contain"
+          className="z-[-1]"
+        />
+
+        <div className="z-20 ml-20 mt-14 w-[450px]">
+          <h1 className="py-3 text-4xl font-semibold tracking-wide text-black">
+            Illustrations
+          </h1>
+        </div>
+        <div className="absolute bottom-0 right-0 z-10 mx-10 max-w-4xl pb-6">
+          <div className="w-full pt-20">
+            <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+              <Link href="#">
+                <Button
+                  name={"Find Out More"}
+                  type={"button"}
+                  className={"border border-white bg-black text-white"}
+                />
+              </Link>
+              <p className="text-sm">
+                When you work with us, you know exactly what each case will
+                cost. No Surprises. It is important that trial and litigation
+                graphics be affordable so that all law firms, regardless of
+                their size or resources, can have access to these powerful
+                tools. This will ensure that all cases, large or small, have the
+                opportunity to receive the best possible representation to tell
+                their story.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </main>
