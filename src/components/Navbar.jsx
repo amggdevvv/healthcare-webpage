1b5615a9a3ec072bbd3359f176eae7c4945a65cc
immadsid <immadsiddiqui86@gmail.com>
2024-04-16 21:42:38 +0500

    Update phone number on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,15 +19,19 @@ const Navbar = () => {
         </p>
       </div>
       <div className="flex items-center justify-between bg-white px-8 py-1 lg:px-24">
-        <Link href="/" className="flex items-center justify-center gap-4">
-          <Image
-            width={50}
-            height={50}
-            src="/images/main-logo.webp"
-            alt="Logo"
-          />
-          <p className="text-xs font-semibold">+1 (866) 520-9939</p>
-        </Link>
+        <div className="flex items-center justify-center gap-4">
+          <Link href="/">
+            <Image
+              width={50}
+              height={50}
+              src="/images/main-logo.webp"
+              alt="Logo"
+            />
+          </Link>
+          <a href="tel:+1 (415) 332-9100" className="text-xs font-semibold">
+            +1 (415) 332-9100
+          </a>
+        </div>
 
         <div className="hidden font-semibold lg:flex">
           <ul className="flex list-none flex-wrap gap-5 text-xs uppercase tracking-widest">
@@ -68,45 +72,45 @@ const Navbar = () => {
             //             absolute right-0 top-32 z-10 mx-4 my-2 min-w-[380px] rounded-xl
             //             p-6 text-white bg-white border border-gray-200`}
 
-            className={`absolute right-0 z-20 mx-auto my-2 min-w-[380px] bg-white overflow-hidden text-white transition-all duration-300 ${
-              menuToggle ? "translate-y-0 top-32 " : "translate-y-[-120%] top-0"
+            className={`absolute right-0 z-20 mx-auto my-2 min-w-[380px] overflow-hidden bg-white text-white transition-all duration-300 ${
+              menuToggle ? "top-32 translate-y-0 " : "top-0 translate-y-[-120%]"
             }`}
           >
-            <ul className="flex list-none flex-col items-start justify-end over tracking-wider">
+            <ul className="over flex list-none flex-col items-start justify-end tracking-wider">
               <li
-                onClick={()=>setMenuToggle(!menuToggle)}
-                className="cursor-pointer text-[16px] font-medium text-white bg-[#3f444b] w-full px-5 py-2"
+                onClick={() => setMenuToggle(!menuToggle)}
+                className="w-full cursor-pointer bg-[#3f444b] px-5 py-2 text-[16px] font-medium text-white"
               >
                 <Link href={"/"}>Home </Link>
               </li>
               <li
-                onClick={()=>setMenuToggle(!menuToggle)}
-                className="cursor-pointer text-[16px] font-medium text-gray-500 w-full px-5 py-2 hover:bg-[#3f444b] hover:text-white"
+                onClick={() => setMenuToggle(!menuToggle)}
+                className="w-full cursor-pointer px-5 py-2 text-[16px] font-medium text-gray-500 hover:bg-[#3f444b] hover:text-white"
               >
                 <Link href="/subscription-plans">Subscription Plans</Link>
               </li>
               <li
-                onClick={()=>setMenuToggle(!menuToggle)}
-                className="cursor-pointer text-[16px] font-medium text-gray-500 w-full px-5 py-2 hover:bg-[#3f444b] hover:text-white"
+                onClick={() => setMenuToggle(!menuToggle)}
+                className="w-full cursor-pointer px-5 py-2 text-[16px] font-medium text-gray-500 hover:bg-[#3f444b] hover:text-white"
               >
                 <Link href="/articles">Articles</Link>
               </li>
 
               <li
-                onClick={()=>setMenuToggle(!menuToggle)}
-                className="cursor-pointer text-[16px] font-medium text-gray-500 w-full px-5 py-2 hover:bg-[#3f444b] hover:text-white"
+                onClick={() => setMenuToggle(!menuToggle)}
+                className="w-full cursor-pointer px-5 py-2 text-[16px] font-medium text-gray-500 hover:bg-[#3f444b] hover:text-white"
               >
                 <Link href="/our-client">Our Clients</Link>
               </li>
               <li
-                onClick={()=>setMenuToggle(!menuToggle)}
-                className="cursor-pointer text-[16px] font-medium text-gray-500 w-full px-5 py-2 hover:bg-[#3f444b] hover:text-white"
+                onClick={() => setMenuToggle(!menuToggle)}
+                className="w-full cursor-pointer px-5 py-2 text-[16px] font-medium text-gray-500 hover:bg-[#3f444b] hover:text-white"
               >
                 <Link href="/portfolios">Portfolio</Link>
               </li>
               <li
-                onClick={()=>setMenuToggle(!menuToggle)}
-                className="cursor-pointer text-[16px] font-medium text-gray-500 w-full px-5 py-2 hover:bg-[#3f444b] hover:text-white"
+                onClick={() => setMenuToggle(!menuToggle)}
+                className="w-full cursor-pointer px-5 py-2 text-[16px] font-medium text-gray-500 hover:bg-[#3f444b] hover:text-white"
               >
                 <Link href="/contact">Contact</Link>
               </li>
